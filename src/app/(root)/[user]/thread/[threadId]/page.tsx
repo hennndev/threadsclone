1688955db1508cfb1988742f267f6a409660797d
Hendra <hendrapermanadev@gmail.com>
2023-09-16 01:33:50 +0700

    bug-fixed

---
+++ src/app/(root)/[user]/thread/[threadId]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ThreadDetail = () => {
+  return (
+    <section className="w-full flex-center p-5">
+      <div className="w-[550px]">
+        <h1>Thread Detail</h1>
+      </div>
+    </section>
+  )
+}
+
+export default ThreadDetail
