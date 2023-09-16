e87207166dc3d41b07b2aad6168322ed61026f1b
Hendra <hendrapermanadev@gmail.com>
2023-09-16 11:59:22 +0700

    big-fixed

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,10 @@
-import { connectDB } from "@/lib/mongoose";
-import { NextResponse } from "next/server";
-import { currentUser } from "@clerk/nextjs";
-import { Users } from "@/lib/models/users.model";
+import { connectDB } from "@/lib/mongoose"
+import { NextResponse } from "next/server"
+import { currentUser } from "@clerk/nextjs"
+import { Users } from "@/lib/models/users.model"
 
 export async function GET() {
+  await connectDB()
   const userLoggedIn = await currentUser()
   try {
     const user = await Users.findOne({id: userLoggedIn?.id})
